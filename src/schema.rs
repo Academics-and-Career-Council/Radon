7a976fcc6cd4fd7fd03001e8564bd62a0e5cef85
vakshit <akshitv18@gmail.com>
2022-02-28 04:27:12 +0530

    add logging to delete_resource

--- src/schema.rs
+++ src/schema.rs
@@ -3,7 +3,46 @@ use crate::{
     model::{Category, Database, NewObject, Object, Resources, ResourcesFrame, Wings},
     Document, ObjectId, MONGO_DATABASE,
 };
+use chrono::{offset::Local, DateTime};
 use juniper::FieldResult;
+use mongodb::error::Error;
+use mongodb::results::DeleteResult;
+use std::time::SystemTime;
+
+struct Log {
+    service_name: String,
+    status_code: i32,
+    severity: String,
+    msg_name: String,
+    msg: String,
+    invoked_by: String,
+    result: String,
+    timestamp: String,
+}
+
+impl Log {
+    fn new(
+        // service_name: String,
+        status_code: i32,
+        severity: String,
+        msg_name: String,
+        msg: String,
+        invoked_by: String,
+        result: String,
+        timestamp: String,
+    ) -> Self {
+        Self {
+            service_name: "radon".to_string(),
+            status_code,
+            severity,
+            msg_name,
+            msg,
+            invoked_by,
+            result,
+            timestamp,
+        }
+    }
+}
 
 pub struct Query;
 #[juniper::graphql_object(
@@ -14,6 +53,7 @@ impl Query {
     #[graphql(arguments(wing(description = "wing of AnC")))]
     fn get_resources_by_wing(wing: String) -> juniper::FieldResult<Vec<Resources>> {
         let db = Database::new();
+
         Ok(db.get_resources(wing))
     }
 
@@ -72,23 +112,23 @@ impl Mutation {
                     None,
                 )
                 .expect(&format!("adding to array failed for {}", heading));
-            Ok(Object {
+            return Ok(Object {
                 id: inserted_id,
                 name: new_object.name,
                 category: new_object.category,
                 link: new_object.link,
-            })
+            });
         } else {
             let resource_frame = doc! {"wing": wing, "order": order.parse::<i32>().unwrap() , "title":heading.clone(), "category" : "document", "object_ids": vec![inserted_id] };
             resources_db
                 .insert_one(resource_frame, None)
                 .expect(&format!("adding {} failed", heading));
-            Ok(Object {
+            return Ok(Object {
                 id: "inserted_id".to_string(),
                 name: new_object.name,
                 category: new_object.category,
                 link: new_object.link,
-            })
+            });
         }
     }
 
@@ -111,6 +151,11 @@ impl Mutation {
     }
 
     fn delete_objects(heading_id: String, object_id: String) -> FieldResult<bool> {
+        // for logging
+        let system_time = SystemTime::now();
+        let datetime: DateTime<Local> = system_time.into();
+        let timestamp: String = datetime.format("D%d/%m/%YT%T").to_string();
+
         let resources_db = MONGO_DATABASE.collection::<ResourcesFrame>("resources");
         let objects_db = MONGO_DATABASE.collection::<Object>("objects");
         let found_resource = resources_db
@@ -120,29 +165,112 @@ impl Mutation {
             )
             .expect(&format!("find failed for {}", heading_id))
             .unwrap();
+
         if found_resource.object_ids.len() == 1 {
-            resources_db
-                .delete_one(
-                    doc! {"_id": ObjectId::parse_str(heading_id.clone()).unwrap()},
-                    None,
-                )
-                .expect(&format!("find failed for {}", heading_id));
-        } else {
-            resources_db
-                .update_one(
-                    doc! {"_id": ObjectId::parse_str(heading_id.clone()).unwrap()},
-                    doc! { "$pullAll": { "object_ids": [ object_id.clone() ] } },
-                    None,
-                )
-                .expect(&format!("delete failed for {}", heading_id));
-        }
-        objects_db
-            .delete_one(
-                doc! {"_id": ObjectId::parse_str(object_id.clone()).unwrap()},
+            let delete_result: Result<DeleteResult, Error> = resources_db.delete_one(
+                doc! {"_id": ObjectId::parse_str(heading_id.clone()).unwrap()},
                 None,
-            )
-            .expect(&format!("delete failed for {}", object_id));
-        Ok(true)
+            );
+            // .expect(&format!("find failed for {}", heading_id));
+            match delete_result {
+                Ok(_) => {
+                    // creating a logging struct
+                    Log::new(
+                        200,
+                        "crit".to_string(),
+                        format!("Resource Block id {{resource_id}}, deleted by {{user_id/email}}"),
+                        format!(
+                            "Resource Block id {{resource_id}} deleted by {{user_id/email}} at {}",
+                            datetime.format("%d/%m/%y %T").to_string()
+                        ),
+                        format!("{{user_id/email}}"),
+                        "success".to_string(),
+                        timestamp.clone(),
+                    );
+                }
+                Err(err) => {
+                    // creating a logging struct
+                    Log::new(500,
+                        "crit".to_string(),
+                        format!("Deletion of resource block id {{resource_id}} failed"), 
+                        format!(
+                        "Deletion of resource block id {{resource_id}} by {{user_id/email}} failed at {}. Reson: {}",
+                        datetime.format("%d/%m/%y %T").to_string(), err.to_string()),
+                        format!("{{user_id/email}}"), "failure".to_string(),
+                        timestamp.clone());
+                }
+            }
+        } else {
+            let resource_update_result = resources_db.update_one(
+                doc! {"_id": ObjectId::parse_str(heading_id.clone()).unwrap()},
+                doc! { "$pullAll": { "object_ids": [ object_id.clone() ] } },
+                None,
+            );
+            // .expect(&format!("delete failed for {}", heading_id));
+            match resource_update_result {
+                Ok(_) => {
+                    // creating a logging struct
+                    Log::new(
+                        200,
+                        "crit".to_string(),
+                        format!("Resource Block id {{resource_id}}, updated by {{user_id/email}}"),
+                        format!(
+                            "Resource Block id {{resource_id}} updated by {{user_id/email}} at {}",
+                            datetime.format("%d/%m/%y %T").to_string()
+                        ),
+                        format!("{{user_id/email}}"),
+                        "success".to_string(),
+                        timestamp.clone(),
+                    );
+                }
+                Err(err) => {
+                    // creating a logging struct
+                    Log::new(500,
+                            "crit".to_string(),
+                            format!("Edit resource block id {{resource_id}} failed"), 
+                            format!(
+                            "Edit resource block id {{resource_id}} by {{user_id/email}} failed at {}. Reson: {}",
+                            datetime.format("%d/%m/%y %T").to_string(), err.to_string()),
+                            format!("{{user_id/email}}"), "failure".to_string(),
+                            timestamp.clone());
+                }
+            }
+        }
+        let object_delete_result = objects_db.delete_one(
+            doc! {"_id": ObjectId::parse_str(object_id.clone()).unwrap()},
+            None,
+        );
+        // .expect(&format!("delete failed for {}", object_id));
+
+        match object_delete_result {
+            Ok(_) => {
+                // creating a logging struct
+                Log::new(
+                    200,
+                    "crit".to_string(),
+                    format!("Object id {{resource_id}}, deleted by {{user_id/email}}"),
+                    format!(
+                        "Object id {{resource_id}} deleted by {{user_id/email}} at {}",
+                        datetime.format("%d/%m/%y %T").to_string()
+                    ),
+                    format!("{{user_id/email}}"),
+                    "success".to_string(),
+                    timestamp.clone(),
+                );
+            }
+            Err(err) => {
+                // creating a logging struct
+                Log::new(500,
+                        "crit".to_string(),
+                        format!("Deletion of Object id {{resource_id}} failed"), 
+                        format!(
+                        "Deletion of resource block id {{resource_id}} by {{user_id/email}} failed at {}. Reson: {}",
+                        datetime.format("%d/%m/%y %T").to_string(), err.to_string()),
+                        format!("{{user_id/email}}"), "failure".to_string(),
+                        timestamp.clone());
+            }
+        }
+        return Ok(true);
     }
 }
 
